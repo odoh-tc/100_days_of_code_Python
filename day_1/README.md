e614e7b09fd2f78649e86b7522333531b20e546d
Tochukwu Odoh <cashearncrypto@gmail.com>
2024-01-05 21:12:33 +0100

    band name generator

---
+++ day_1/README.md
@@ -0,0 +1,5 @@
+- Printing to the console in Python
+- String Manipulation
+- Python input function
+- Variables and Variable naming
+- Band name generator project
---
+++ day_1/band_name_generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band name Generator")
+
+city = input("which city did you grow up in?\n")
+
+pet_name = input("what is the name of your pet?\n")
+
+print("Your band name could be " + city + " " + pet_name)
