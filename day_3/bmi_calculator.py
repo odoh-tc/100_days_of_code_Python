36dfc0a3fef680ba95308fe2b561cfffcf205b65
Tochukwu Odoh <cashearncrypto@gmail.com>
2024-01-06 16:32:48 +0100

    bmi_calculator

--- day_3/bmi_calculator.py
+++ day_3/bmi_calculator.py
@@ -5,10 +5,10 @@ bmi_index = weight / (height ** 2)
 if bmi_index < 18.5:
     print(f"Your BMI is {bmi_index} kg/m², you are underweight.")
 elif 18.5 < bmi_index < 25:
-    print(f"Your BMI is {bmi_index} kg/m², you are have a normal weight.")
+    print(f"Your BMI is {bmi_index} kg/m², you have a normal weight.")
 elif 25 <= bmi_index < 30:
-    print(f"Your BMI is {bmi_index} kg/m², you are have a  slightly overweight.")
+    print(f"Your BMI is {bmi_index} kg/m², you are slightly overweight.")
 elif 30 <= bmi_index < 35:
-    print(f"Your BMI is {bmi_index} kg/m², you are have a obese.")
+    print(f"Your BMI is {bmi_index} kg/m², you are obese.")
 else:
-    print(f"Your BMI is {bmi_index} kg/m², you are have a clinically obese")
+    print(f"Your BMI is {bmi_index} kg/m², you are clinically obese")
